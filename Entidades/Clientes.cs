48057ad78babeca013f1ee991feffa59b9ca345f
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2020-02-18 20:15:36 -0600

    Creación de entidades

---
+++ Entidades/Clientes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class clientes
+    {
+        public int ID { get; set; }
+        public string NOMBRE_COMPLETO { get; set; }
+        public double IDENTIFICACION { get; set; }
+        public double TELEFONO { get; set; }
+    }
+}
